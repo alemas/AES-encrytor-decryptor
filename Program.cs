75ba11b4c43594e1783beabf2bfe2d7737b69178
Mateus Eckert Reckzeagel <mateus.eckert@poa01.local>
2019-10-03 14:15:26 -0300

    Started basic user inputs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.IO;
 
-namespace AES_encrytor_decryptor
+namespace NumbersInCSharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length >= 3) {
+
+                string filepath = args[1];
+
+                if (!File.Exists(filepath)) {
+                    Console.WriteLine("File doesn't exist");
+                    return;
+                }
+
+                // Encriptar
+                if (args[0] == "-e") {
+                    Console.WriteLine("Encrypt operation");
+
+                // Decriptar
+                } else if (args[0] == "-d") {
+                    Console.WriteLine("Decrypt operation");
+
+                } else  {
+                    Program.ShowHelp();
+                }
+            } else {
+                Program.ShowHelp();
+            }
+
+            for (int i = 1; i < args.Length; i++)
+            {
+                string arg = args[i];
+                if (arg == "-e") {
+
+                }
+            }
+            
+        }
+
+        private static void ShowHelp() 
+        {
+            Console.WriteLine("Usage: dotnet run Program [-e|-d] text-filepath key-filepath [-cbc|-ctr]");
+        }
+
+        private static string Encrypt(string text) 
+        {
+
+            return "";
         }
     }
 }
