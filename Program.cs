f9e425d1680067aea4f07c45be36bbb1842433a5
Mateus Eckert Reckzeagel <mateus.eckert@poa01.local>
2019-10-03 14:14:12 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AES_encrytor_decryptor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
